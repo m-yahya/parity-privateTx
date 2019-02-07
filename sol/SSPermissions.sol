919e373be1263d7968c92b84995d6bd27f0d107a
Muhammad Yahya <yahya_gis@hotmail.com>
2019-02-07 17:49:28 +0100

    part 4 completed

--- sol/SSPermissions.sol
+++ sol/SSPermissions.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
 contract SSPermissions {
-    address alice = 0xf026a4c280386e3a950ceb9c2a64f77c24f8d170;
+    address alice = 0x08bcf855503571c673c28fdb31add07c433b005e;
     address bob = 0x3f4ef948fd7307e82b67bb438ad9e14e6aea7c4d;
 
     // both Alice and Bob can access any document
